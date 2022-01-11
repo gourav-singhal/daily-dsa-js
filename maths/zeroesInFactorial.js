d676ee49d836dc519932a36b32aba718797f7de2
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-01-11 21:57:12 +0530

    Count number of zeroes
    Added a better approach to count number of zeroes in factorial of a integer.

--- maths/zeroesInFactorial.js
+++ maths/zeroesInFactorial.js
@@ -5,6 +5,8 @@
 
 /**
 * 1. Brute force (iterative method)
+* Problem with this solution is that we may get a stack overflow error
+* for very small integer values computing factorials.
 */
 
 function countZeroes(n) {
@@ -22,6 +24,23 @@ function countZeroes(n) {
     fact = fact / 10;
   }
   
+  return res;
+}
+
+
+/**
+* 2. Better approach
+* Compute Number of 2's and 5's in prime factors of n
+* Since number of 2's is always greater than number of 5's we will 
+* only compute number of 5's and that will be our answer in O(log(n)) time 
+* as compared to above O(n) solution
+*/
+
+function countZeroes(n) {
+  let res = 0;
+  for (let i = 5; i <= n; i = i * 5) {
+    res = res + Math.floor(n/i);
+  }
   return res;
 }
 
