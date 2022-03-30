0d30d09fb8352a2ec8dcbe0e9ffae0d3955d353f
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-03-30 20:35:29 +0530

    Update countNumberDecoding.js
    Added DP approach

--- countNumberDecoding.js
+++ countNumberDecoding.js
@@ -34,3 +34,29 @@ function countDecoding(digits, n) {
 }
 
 
+/**
+2. Dynamic programming O(n)
+*/
+
+
+function countDecodingDP(digits, n) {
+  const count = new Array(n+1);
+  
+  count[0] = 1;
+  count[1] = 1;
+  
+  for (let i = 0; i <= n; i++) {
+    count[i] = 0;
+    
+    if (digits[i-1] > 0)
+      count[i] = count[i-1];
+    
+    if((digits[i-2] == 1 || digits[i-2] == 2) && digits[i-1] < 7)
+      count += count[i-2];
+  }
+  return count[n];
+}
+
+
+
+
