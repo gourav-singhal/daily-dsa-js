58a86ce3ff57cd74e1344299671ce506ba653fa0
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-02 15:34:04 +0530

    Update fibonacci.js
    Added Dynamic programming solution using memoization

--- fibonacci.js
+++ fibonacci.js
@@ -12,3 +12,17 @@ function fib(n) {
   return fib(n-1) + fib(n-2);
 }
 
+/**
+2. Dynamic programming solution (Memoization)
+*/
+
+function fib(n, memo = {}) {
+  if (n in memo) return memo[n];
+  
+  if(n <= 1) return 0;
+  if(n == 2) return 1;
+  
+  memo[n] = fib(n-1, memo) + fib(n-2, memo);
+  return memo[n];
+}
+
