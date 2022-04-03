04b3105c09776c628587abf6fd350519505da01b
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-03 16:21:53 +0530

    Update canSum.js
    Added DP approach using memoization

--- dp/canSum.js
+++ dp/canSum.js
@@ -22,4 +22,25 @@ function canSum(target, numbers) {
   }
   return false;
 }
+
+
+/**
+2. DP O(m*n)
+*/
+
+function canSum(target, numbers, memo=[]) {
+  if (target in memo) return memo[target];
+  if (target === 0) return true;
+  if (target < 0) return false;
   
+  for (let num of numbers) {
+    const remainder = target - num;
+    
+    if (canSum(remainder, numbers, memo) === true) {
+      memo[target] = true;
+      return true;
+    }
+  }
+  memo[target] = false;
+  return false;
+}
