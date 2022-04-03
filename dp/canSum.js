17edca92daedab96697bab692d0d47bc7ba3de7b
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-03 16:14:21 +0530

    Create canSum.js
    Added Brute force approach

---
+++ dp/canSum.js
@@ -0,0 +1,25 @@
+// Given a target and an array find if it is possible to 
+// obtain the target by summing elements of array
+// elements may be repeated
+
+// Example:- canSum(7. [2.3.4,7]) --> true
+// canSum(7, [2,4]) --> false
+// canSum(7, [2,3]) --> true (2+2+3)
+
+/**
+1. Brute force (O(n^m))
+*/
+
+function canSum(target, numbers) {
+  if (target === 0) return true;
+  if (target < 0) return false;
+  
+  for (let num of numbers) {
+    const remainder = target - num;
+    if(canSum(remainder, numbers) === true) {
+      return true;
+    }
+  }
+  return false;
+}
+  
