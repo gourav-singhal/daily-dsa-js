002977c0ca0d21d1da3a15c028a0753e6e368a12
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-06 20:32:08 +0530

    Update bestSum.js
    Added dynamic approach using tabulation

--- dp/bestSum.js
+++ dp/bestSum.js
@@ -59,5 +59,25 @@ function bestSum(target, numbers, memo={}) {
   return memo[target];
 }
 
+
+/**
+* 2. DP approach -- tabulation -- O(m^2 * n)
+*/
   
-  
+  function bestSum(target, numbers) {
+    const table = Array(target+1).fill(null);
+    table[0] = [];
+    
+    for (let i = 0; i <= target; i++) {
+      if (table[i] !== null) {
+        for (let num of numbers) {
+          const combination = [ ...table[i], num];
+          if ((!table[i+num]) || table[i+num].length > combination.length) {
+            table[i+num] = combination;
+          }
+        }
+      }
+    }
+    return table[target];
+  }
+    
