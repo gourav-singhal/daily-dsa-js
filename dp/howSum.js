638f4917db1d9fa524475de15287d466df991074
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-06 19:08:28 +0530

    Update howSum.js
    Added Dynamic programming approach using tabulation

--- dp/howSum.js
+++ dp/howSum.js
@@ -46,3 +46,22 @@ function howSum(target, numbers, memo={}) {
   memo[target] = null
   return memo[target];
 }
+
+
+/**
+* 2. DP tabulation O(n*m^2)
+*/
+function howSum(target, numbers) {
+  const table = Array(target+1).fill(null);
+  table[0] = [];
+  
+  for (let i = 0; i <= target; i++) {
+    if (table[i] !== null) {
+      for (let num of numbers) {
+        table[i+num] = [...table[i], num];
+      }
+    }
+  }
+  return table[target];
+}
+
