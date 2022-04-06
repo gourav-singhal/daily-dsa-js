a214ea9a5f5eca002559b9f9cc515e741217d78e
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-06 14:28:51 +0530

    Create constructWays.js
    Added brute force approach

---
+++ dp/constructWays.js
@@ -0,0 +1,28 @@
+// Given a string "target" and an array of strings "wordBank"
+// Check if it is posssible to construct the target string using sub-strings 
+// from wordBank array. Use subArray as many time as you need.
+// Return 2D array of substrings containing sequences to get target.
+
+// For Example:- canConstruct("abcde", ["ab", "bc", "cde", "dce"]) --> 1
+// canConstruct("skateboard", ["skate", "sk", "boar", "ate"]) --> 0
+
+/**
+* 1. Brute force O(n^m * m)
+*/
+
+
+function constructWays(target, wordBank) {
+  if (target === '') return [[]];
+  
+  let result = [];
+  for (let word of wordBank) {
+    if (target.startsWith(word) {
+        const suffix = target.slice(word.length);
+        const suffixWays = constructWays(suffix, wordBank);
+        
+        const finalSuffixWays = suffixWays.map(way => [word, ...way]);
+        result.push(...finalSuffixWays);
+  }
+}
+return result;
+}
