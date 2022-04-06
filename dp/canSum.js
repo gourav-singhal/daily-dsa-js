7b93efe15ff37927f939acf17501d514d8b21e00
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-06 18:48:45 +0530

    Update canSum.js
    Added Dynamic approach using tabulation

--- dp/canSum.js
+++ dp/canSum.js
@@ -44,3 +44,22 @@ function canSum(target, numbers, memo=[]) {
   memo[target] = false;
   return false;
 }
+
+
+/**
+2. DP tabulation O(m*n)
+*/
+
+function canSum(target, numbers) {
+  const table = Array(target + 1).fill(false);
+  table[0] = true;
+  
+  for (let i = 0; i <= target; i++) {
+    if (table[i] === true) {
+      for(let num of numbers) {
+        table[i+num] = true;
+      }
+    }
+  }
+  return table[target];
+}
