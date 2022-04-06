46727d5b1afa973c417674ef998da178ca750848
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-06 16:41:29 +0530

    Update fibonacci.js
    Added DP approach using tabulation

--- fibonacci.js
+++ fibonacci.js
@@ -26,3 +26,18 @@ function fib(n, memo = {}) {
   return memo[n];
 }
 
+/**
+3. Dynamic programming solution (Tabulation)
+*/
+
+function fib(n) {
+  const table = new Array(n+1).fill(0);
+  table[1] = 1;
+  
+  for (let i = 0; i <= n; i++) {
+    table[i+1] += table[i];
+    table[i+2] += table[i];
+  }
+  return table[n];
+}
+
