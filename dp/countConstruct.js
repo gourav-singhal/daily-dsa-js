c990ea4a819ba7b32a98f3eec9a0759105513cda
Gourav Singhal <gourav.singhal.2409@gmail.com>
2022-04-07 11:09:45 +0530

    Update countConstruct.js
    Added DP approach using tabulation

--- dp/countConstruct.js
+++ dp/countConstruct.js
@@ -45,5 +45,22 @@ function countCostruct(target, wordBank, memo={}) {
    }
    
    
-   
+   /**
+   * 2. DP -- tabulation -- O (m^2 * n) 
+   */
+
+function countConstruct(target, wordBank) {
+    const table = Array(target.length + 1).fill(0);
+    table[0] = 1;
+    
+    for (let i = 0; i <= target.length; i++) {
+        for (let word of wordBank) {
+            if (target.slice(i, i + word.length) === word) 
+            table[i + word.length] += table[i];
+        }
+    }
+    return table[target.length];
+}
+
+  
       
